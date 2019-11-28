1789950e9bdb245788273c2ac79023ab0984c2e9
Alexey Isotov <alexisotov@gmail.com>
2019-11-28 14:41:50 +0200

    css

--- src/components/item.vue
+++ src/components/item.vue
@@ -2,18 +2,22 @@
 <div @mouseover="showButton=true" @mouseleave="showButton=false" class="task-container">
   <div>
     <div class="task-item">
-      <input v-bind:id="id" type="checkbox" v-bind:checked="done" @change="$emit('complete-task')" />
-      <label v-bind:for="id" class="show-info"><strong>{{task}}</strong></label>
+      <div>
+        <input v-bind:id="id" type="checkbox" v-bind:checked="done" @change="completeTask" />
+        <label v-bind:for="id" class="show-info"><strong v-bind:class="{donetask:done}">{{task}}</strong></label>
+      </div>
       <transition name="slide-fade">
-        <button v-if="showButton" @click="showMore">?</button>
+        <button v-if="showButton" @click="showMore" class="btn-show">?</button>
       </transition>
     </div>
-    <transition name="fade">
-      <div v-if="show">
-      <p>Date:{{date}}</p>
-      <p>Until:{{dateTo}}</p>
-      </div>
-    </transition>
+    <div class="info">
+      <transition name="fade">
+        <div v-if="show">
+        <p>Date:{{date}}</p>
+        <p>Until:{{dateTo}}</p>
+        </div>
+      </transition>
+    </div>
   </div>
   <div class="btn-container">
     <button @click="$emit('del-item', id)" class="del-button">&times;</button>
@@ -34,12 +38,16 @@ export default {
     return{
       showButton:false,
       show: false,
-      complete: false
+      complete: false,
       }
   },
   methods: {
     showMore() {
       this.show=!this.show
+    },
+    completeTask(){
+      this.line = "line-through"
+      this.$emit('complete-task')
     }
   }
 }
@@ -50,22 +58,33 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.task-item {
+.task-item, .info {
   display:flex;
+  justify-content: start;
 }
 .show-info {
   cursor:pointer;
+}
+.btn-show {
+  border:0;
+  border-radius: 50%;
+  background: dodgerblue;
+  margin-left:10px;
+  color: white;
 }
 .btn-container {
 
 }
 .slide-fade-enter-active {
-  transition: all .3s ease;
+  transition: all .2s ease;
 }
 .slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all .1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 .slide-fade-enter, .slide-fade-leave-to {
   transform: translateX(100px);
+}
+.donetask {
+  text-decoration: line-through;
 }
 </style>
